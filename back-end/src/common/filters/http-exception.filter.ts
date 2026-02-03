455eda7c7c808c45b769c627d9c54fbc10c38d39
medlaq777 <m.laqsoumi@gmail.com>
2026-02-03 14:27:13 +0100

    FLOW-29 make the filter file and Implementing the Error Handling Global

---
+++ back-end/src/common/filters/http-exception.filter.ts
@@ -0,0 +1,24 @@
+import {
+  ArgumentsHost,
+  ExceptionFilter,
+  HttpException,
+  Catch,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    const status = exception.getStatus();
+    const message = exception.message;
+
+    response.status(status).json({
+      statusCode: status,
+      message,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
