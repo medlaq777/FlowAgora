d93bd9fce7406111ed62b70b02a2514006e8dd61
medlaq777 <m.laqsoumi@gmail.com>
2026-02-03 12:30:46 +0100

    FLOW-26 Config the mongoDb connection and also fix TypeScript Config

---
+++ back-end/src/database/database.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI ?? '', {
+      dbName: 'FlowAgora',
+    }),
+  ],
+})
+export class DatabaseModule {}
